c93e86a634fe30a096c4b7c8d9a1be038922a717
Dmitri Beletki <dmitri.beletki@ussoftwaresolutions.us>
2022-10-25 22:33:10 +0300

    clear

--- l8_jsx.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const a: JSX.Element = <div tabIndex={0}>{1 + 1}</div>;
-
-// const bb: JSX.Element = React.createElement('div', { tabIndex: 0 }, 1 + 1);
-
-const a23 = b as string;
