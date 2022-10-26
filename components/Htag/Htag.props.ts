a0fadd3e193a566c739329c4ccf397fa2a5e3526
Dmitri Beletki <dmitri.beletki@ussoftwaresolutions.us>
2022-10-26 13:44:32 +0300

    add button

---
+++ components/Htag/Htag.props.ts
@@ -0,0 +1,6 @@
+import React, { ReactNode } from "react";
+
+export interface HtagProps {
+    tag: 'h1'|'h2'|'h3';
+    children: ReactNode
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import { Button, Htag } from "../components";
+
+export default function Home():JSX.Element {
   return (
-    <div>
-      aasas
-    </div>
+    <>
+      <Htag tag='h1'>Text</Htag>
+      <Button appearance="primary">button</Button>
+      <Button appearance="ghost">button ghost</Button>
+    </>
   )
 }
