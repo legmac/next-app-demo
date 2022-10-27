08b65f8c4f963b4e6fe9e5a7e5f26cd701afe1bd
Dmitri Beletki <dmitri.beletki@ussoftwaresolutions.us>
2022-10-27 14:56:23 +0300

    addopt layout

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -8,18 +8,15 @@ import { FunctionComponent } from "react"
 
 const Layout = ({children}: LayoutProps):JSX.Element => {
     return (
-        <>
-        <Header/>
-        <div>
-            <Sidebar />
-            <div>
-                {children}
-            </div>
+        <div className={styles.wrapper}>
+        <Header className={styles.header}/>
+        <Sidebar className={styles.sidebar}/>
+        <div className={styles.body}>
+            {children}
         </div>
-        <Footer/>
-        </>
+        <Footer className={styles.footer}/>
+        </div>
     )
-
 }
 
 export const withLayout = <T extends Record<string, unknown>>(Component: FunctionComponent<T>) =>{
