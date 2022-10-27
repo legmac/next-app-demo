6aa557321ea35fe57b244620158d2387b4658cc5
Dmitri Beletki <dmitri.beletki@ussoftwaresolutions.us>
2022-10-27 10:53:31 +0300

    add withLayout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,15 @@
 import { useEffect, useState } from "react";
 import { Button, Htag, P, Rating, Tag } from "../components";
+import { Layout, withLayout } from "../layout/Layout";
 
-export default function Home():JSX.Element {
-
-  // const [counter, setCounter] = useState<number>(0);
-  // useEffect(()=>{
-  //   // console.log('Counter %s', 'counter');
-  //   console.log("Counter " + counter)
-  //   return function cleanup(){
-  //     console.log("Unmount " + counter)
-  //   }
-  // } );
-
-  // useEffect(()=>{
-  //   console.log("Mounted")
-  // }, [] );
-
+// export default function Home():JSX.Element {
+ function Home():JSX.Element {
   const [rating, setRaiting] =  useState<number>(4);
 
   return (
     <>
       <Htag tag='h1'>"\counter\"</Htag>
-      <Button appearance="primary" arrow="right" onClick={()=> setCounter(x => x+1)}>button</Button>
+      <Button appearance="primary" arrow="right">button</Button>
       <Button appearance="ghost"  arrow="down">button ghost</Button>
 
       <P size="s">Small.</P>
@@ -38,3 +26,5 @@ export default function Home():JSX.Element {
     </>
   )
 }
+
+export default withLayout(Home);
