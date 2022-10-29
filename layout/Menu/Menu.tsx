a1224ad9c5d1f1557ba375c727e8f07b4a340d68
Dmitri Beletki <dmitri.beletki@ussoftwaresolutions.us>
2022-10-30 02:22:40 +0300

    fix static paths

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,22 +1,11 @@
 import styles from './Menu.module.css'
-import { useContext, useEffect } from 'react'
+import { useContext } from 'react'
 import { AppContext } from '../../context/app.context'
 import { FirstLevelMenuItem, PageItem } from '../../interfaces/menu.interface'
-import CoursesIcon from './icons/courses.svg'
-import ServicesIcon from './icons/services.svg'
-import BooksIcon from './icons/books.svg'
-import ProductsIcon from './icons/products.svg'
-import { TopLevelCategory } from '../../interfaces/page.interface'
 import cn from 'classnames'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-
-const firstLevelMenu: FirstLevelMenuItem[] = [
-    { route: 'courses', name: 'Курсы', icon: <CoursesIcon/>, id: TopLevelCategory.Courses},
-    { route: 'serices', name: 'Сервисы', icon: <ServicesIcon/>, id: TopLevelCategory.Services},
-    { route: 'books', name: 'Книги', icon: <BooksIcon/>, id: TopLevelCategory.Books},
-    { route: 'products', name: 'Товары', icon: <ProductsIcon/>, id: TopLevelCategory.Products}
-]
+import { firstLevelMenu } from '../../helpers/helpers'
 
 export const Menu = ():JSX.Element => {
 const {menu, setMenu, firstCategory: firstCategory} = useContext(AppContext)
---
+++ pages/search.tsx
@@ -0,0 +1,30 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import { MenuItem } from "../interfaces/menu.interface";
+import {  withLayout } from "../layout/Layout";
+import axios from 'axios'
+
+function Search():JSX.Element {
+  return (
+    <>
+        Search
+    </>
+  )
+}
+
+export default withLayout(Search);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async() => {
+  const firstCategory = 0
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', { firstCategory })
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  }
+}
+
+interface HomeProps extends Record<string,unknown>{
+  menu: MenuItem[],
+  firstCategory: number
+}
